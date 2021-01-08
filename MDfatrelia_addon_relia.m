bcf37b95efc024eb8673ad7a97b0282987177e6e
evamschmitt <eva.m.schmitt@ntnu.no>
2021-01-08 11:30:28 +0100

    adjusted to michaels suggestions, now only two uncertainties

--- MDfatrelia_addon_relia.m
+++ MDfatrelia_addon_relia.m
@@ -13,41 +13,34 @@
 
 Ax_start = 0;
 Axstep = 0.1;
-nloop = 200;
+nloop = 198;
 
 Ax_end = nloop*Axstep;
 
 % give simulation time [s]
-runtime = 300;
+runtime = 1200;
 
 % Fatigue specific Inputs (Factors)
 M = 3;              % Factor siehe API RP 2SK - FUER COMMON STUDLESS LINK CHAIN
 K = 316;            % Factor siehe API RP 2SK - FUER COMMON STUDLESS LINK CHAIN (CHECK FOR SEMISUB!)
 R2 = 8167000;       % Minimum Breaking Strength [N] FOR 90MM R4 STUDLESS CHAIN (e.g. from https://ramnas.com/wp-content/uploads/2012/11/Ramnas-Technical-Broschure.pdf )
-R2_base = R2;       % to save original R2 value, when uncertainty is applied later on
+
 
 %% Uncertainty Distribution Factors for Random Variables
 % Give uncertainity distribution factors to determine random values later
 
-% 1. Load = Amplitude -> Weibull Distribution
-Amp_scaleParameter = 3;     % just assumption In Weibull analysis, what exactly is the scale parameter, η (Eta)? And why, at t = η , will 63.21% of the population have failed, regardless of the value of the shape parameter, β (Beta)?
-Amp_shapeParameter = 2;     % just assumption
-
-% 1.b Uncertainty for Simplified Approach to Load (having pre-defined, rounded
-% values)
-mean_SimAppLoad = 0;
-standard_deviation_SimAppLoad = 0.05;
+% 1. Load = Normal Distribution
+Amp_mean = 10;     
+Amp_variance = 3;    
 
 
-% 2. Resistance (Max. endurable fatigue) = reference breaking strength = R2
-mean_R2 = R2;
-standard_deviation_R2 = 0.05; % just assumption for now
-
-
-% 3. Miner Sum (Uncertainty from damage calculation procedure ((summing
-% up)) ) Lognormal
-MinerSum_Mean_of_logarithmic_values = 0;   % needs to be zero so average is 1                % just assumption for now
-MinerSum_standard_derivation_of_logarithmic_values = 0.05; %0.05; % just assumption for now
+% 2. Uncertainty Resistance (how much damage can the system take?)
+% Lognormal Distribution
+% Mu and Sigma parameters therefor
+% the below are calculated for 1 mean and 0.05 variance
+% see matlab webpage help on how to calculate
+mu = -0.0244;
+sigma = 0.2209;
 
 
 %% Get further variables from outputfiles
@@ -63,12 +56,16 @@ runs = 10000; %10000
 Survival_Average = zeros(nls,1);
 Lifetime_Damage_Average = zeros(nls,1);
 
+% delete old excel output file so no numbers are mistaken for the new ones
+delete('Survival_Average.xls');
+delete('Lifetime_Damage_Average.xls');
+
 for j = 1:runs  % how many iterations to I need to reach convergence? Adjust this number accordingly!
     tic
     %% Generate random values:
     
-    % 1. Load = Amplitude -> Weibull Distribution
-        Amp_rand_value = wblrnd(Amp_scaleParameter, Amp_shapeParameter);
+    % 1. Load = Amplitude -> Normal Distribution
+        Amp_rand_value = normrnd(Amp_mean, Amp_variance);
         % Round rand value, so that it fits pre calculated results:
         Amp_rand_value = round(Amp_rand_value, 1);
         % if rand value is larger than max. calculated value, set rand value to
@@ -77,31 +74,11 @@ for j = 1:runs  % how many iterations to I need to reach convergence? Adjust thi
             Amp_rand_value = Ax_end;    
         end
         
-        
-    % 1.b Uncertainty for Simplified Approach to Load (having pre-defined, rounded
-    % values)
-    SimAppLoad_rand_value = lognrnd(mean_SimAppLoad, standard_deviation_SimAppLoad);
-    
 
-    % 2. Resistance (Max. endurable fatigue) = reference breaking strength = R2
-        %R2_rand_value = lognrnd(mean_R2/1000000, standard_deviation_R2); %->doesn't work become infinite, instead:
-        R2_rand_value = lognrnd(0, standard_deviation_R2)*mean_R2; %maybe
-        %isn't accurate, since 
-        %R2_rand_value = lognrnd(mean_R2/1000000, standard_deviation_R2);
-        %-> leads to very high values, is this correct??
-        % Apply randomness to R2 defined (base) value
-        %R2 = R2_rand_value;
-        
-    % 3. Miner Sum
-        MinerSum_rand_value = lognrnd(MinerSum_Mean_of_logarithmic_values, MinerSum_standard_derivation_of_logarithmic_values);
-
-        
-        
-%% Apply random values
-
-
-
-
+    % 2. Resistance (Max. endurable fatigue) rand value (vary around the
+    % total damage 1)
+    resistance_rand = lognrnd(mu,sigma);
+           
 
 %% Calc Fatigue for current set of random values:
 
@@ -110,8 +87,8 @@ for j = 1:runs  % how many iterations to I need to reach convergence? Adjust thi
     MDit = Amp_rand_value/Axstep + 1; %+1 because sheets start from sheet 1, so if amp is roundet zero, it has a sheet.
     MDit = round(MDit); %Maybe round because Sheet function below can take only whole numbers, so the zeros are taken away? does this matter?
 % Then get rainflow count for that iteration and apply uncertainty to it:
-    M_R1 = readmatrix('M_R1.xlsx','Sheet',MDit)*SimAppLoad_rand_value;
-    M_BinCountsVector = readmatrix('M_BinCountsVector.xlsx','Sheet',MDit)*SimAppLoad_rand_value;
+    M_R1 = readmatrix('M_R1.xlsx','Sheet',MDit);
+    M_BinCountsVector = readmatrix('M_BinCountsVector.xlsx','Sheet',MDit);
 
 len_M_R1 = length(M_R1);
 
@@ -154,7 +131,7 @@ Lifetime_Damage = AnnualDamagePerSegment.*25;                    % for 25years r
 Survival = ones(nls,1);                                      % Creates Surival Vector with ones (=survival) as default
 
 for k = 1:nls
-    if Lifetime_Damage(k, 1) > MinerSum_rand_value % MinerSum_rand_value should be around 1, Distribution for Uncertainty Miner Sum
+    if Lifetime_Damage(k, 1) > resistance_rand % value should be around 1
     Survival(k,1) = 0;
     end
 end
